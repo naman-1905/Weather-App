cc1fbc4b31bf6e0e3e7c52ad21fe25e2fb749d4a
naman-1905 <namanoj19@gmail.com>
2025-07-27 22:05:21 +0530

    Updated the Navbar to showcase the temperature and location of the user

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,18 +1,20 @@
 "use client";
 
 import React from 'react';
-import { SunMedium, MapPin } from 'lucide-react';
+import { SunMedium, MapPin, CloudIcon } from 'lucide-react';
 import DateTimeDisplay from './DayDate';
 import useIPLocation from './IPLocation';
+import useWeather from './useWeather';
 
 export default function Navbar() {
-  const { city, country } = useIPLocation();
+  const { city, country, lat, lon } = useIPLocation();
+  const temperature = useWeather(lat, lon);
 
   return (
     <nav className="shadow-sm sticky top-0 bg-white w-full px-4 py-4 z-50">
       <div className="relative h-[80px] w-full max-w-screen-xl mx-auto px-4 flex items-center justify-between">
         
-        {/* LEFT: Logo + Sun */}
+        {/* Weather Icon */}
         <div className="absolute left-4 flex items-center gap-2">
           <h2 className="text-gray-600 text-2xl flex justify-start font-bold whitespace-nowrap">
             Half Skirmish Weather App
@@ -20,17 +22,24 @@ export default function Navbar() {
           <SunMedium className="text-orange-300 h-16 w-16" />
         </div>
 
-        {/* CENTER: Date & Time */}
         <div className="mx-auto">
           <DateTimeDisplay />
         </div>
 
-        {/* RIGHT: Location + Search */}
         <div className="absolute right-4 flex items-center gap-3">
           <MapPin className="text-black h-6 w-6" />
-          <span className="text-black font-bold whitespace-nowrap">
-            {city ? `${city}, ${country}` : country || 'Loading...'}
-          </span>
+          
+          <div className="flex flex-col">
+            <span className="text-black font-bold whitespace-nowrap">
+              {city ? `${city}, ${country}` : country || 'Loading...'}
+            </span>
+
+            <span>
+              <span className="font-semibold text-blue-500">
+                Temperature: {temperature !== null ? `${temperature}°C` : 'Loading...'}
+              </span>
+            </span>
+          </div>
         </div>
       </div>
     </nav>

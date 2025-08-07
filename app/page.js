d6eae77ff8eb59ed6e8ef413fef2076dcbf40a4d
naman-1905 <namanoj19@gmail.com>
2025-08-07 21:12:40 +0530

    Added the 6 Days History Component to Landing Page

--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@
 
 import Navbar from "./components/Navbar"
 import WeatherChart from "./components/24HData";
+import SixDayHistory from "./components/6DayHistory";
 export default function Home() {
 
   return (
     <div>
        <Navbar/>
        <WeatherChart />
+       <SixDayHistory />
     </div>
   );
 }

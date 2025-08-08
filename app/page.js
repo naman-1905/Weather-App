ec6e35a0a9d39d743dc50a601fcc47bfc6b6639e
naman-1905 <namanoj19@gmail.com>
2025-08-08 22:37:09 +0530

    Imported the Weather Summary Card to main page

--- app/page.js
+++ app/page.js
@@ -4,11 +4,13 @@
 import Navbar from "./components/Navbar"
 import WeatherChart from "./components/24HData";
 import SixDayHistory from "./components/6DayHistory";
+import WeatherSummary from "./components/WeatherSummary";
 export default function Home() {
 
   return (
     <div>
        <Navbar/>
+       <WeatherSummary/>
        <WeatherChart />
        <SixDayHistory />
     </div>

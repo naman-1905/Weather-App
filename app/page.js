c29942beb75fc66ecd84cb68720eccab26e5c44e
naman-1905 <namanoj19@gmail.com>
2025-08-22 11:45:13 +0530

    Modified the App to version 2

--- app/page.js
+++ app/page.js
@@ -1,18 +1,11 @@
 'use client'
+import WeatherDashboard from "./v2components/weatherapp";
 
-
-import Navbar from "./components/Navbar"
-import WeatherChart from "./components/24HData";
-import WeatherSummary from "./components/WeatherSummary";
-import SevenDayHistory from "./components/7DayHistory";
 export default function Home() {
 
   return (
     <div>
-       <Navbar/>
-       <WeatherSummary/>
-       <WeatherChart />
-       <SevenDayHistory/>
+      <WeatherDashboard/>
     </div>
   );
 }

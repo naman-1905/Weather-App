c11eb1f7eb8a9b901ba8ad985e241fe39c9095dd
naman-1905 <namanoj19@gmail.com>
2025-08-22 11:45:58 +0530

    Modified the DayDate to just show the day and date and not time

---
+++ app/utils/DayDate.js
@@ -0,0 +1,31 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+function DateTimeDisplay() {
+  const [dateTime, setDateTime] = useState(null);
+
+  useEffect(() => {
+    setDateTime(new Date());
+
+    const interval = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (!dateTime) return null;
+
+  const day = dateTime.toLocaleDateString('en-US', { weekday: 'long' });
+  const date = dateTime.toLocaleDateString('en-US'); // fix locale explicitly
+  const time = dateTime.toLocaleTimeString('en-US'); // fix locale explicitly
+
+  return (
+    <span className="text-white text-lg font-bold items-center flex flex-col gap-1">
+      <span>{day}, {date}</span>
+    </span>
+  );
+}
+
+export default DateTimeDisplay;

36f8bbb90cb5896f3f9be1997d5945fe07b787b8
naman-1905 <namanoj19@gmail.com>
2025-08-23 21:02:59 +0530

    Changed the nextjs config for deployment purposes

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   experimental: {
     turbo: false, // Disable Turbopack to fix font error
   },
-  output: "standalone", // ✅ This is what makes .next/standalone appear
 };
 
 export default nextConfig;
